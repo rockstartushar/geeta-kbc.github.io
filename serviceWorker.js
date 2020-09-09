f75af07959b053aa1a01255f10919b689d4f370a
Tushar Dangayach <54102499+rockstartushar@users.noreply.github.com>
2020-09-09 11:00:23 +0530

    Few edits

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-const staticDevCoffee = "dev-fightagainstcorona-site-v1"
+const staticDevClasses = "dev-classes-site-v1"
 const assets = [
   "/",
   "index.html",
@@ -9,7 +9,7 @@ const assets = [
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
+    caches.open(staticDevClasses).then(cache => {
       cache.addAll(assets)
     })
   )
